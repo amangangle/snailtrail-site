fe3b1b43978371b061ac4bb90d10318e2fae5d89
amangangle <amangangle@gmail.com>
2025-08-11 22:15:42 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["firebasestorage.googleapis.com", "images.unsplash.com"]
+  }
+}
+module.exports = nextConfig
